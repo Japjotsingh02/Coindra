b0a05fdc9463af23f0a1eb0377ec6ca4dfca62aa
Japjotsingh02 <sjapjot2002@gmail.com>
2025-09-02 17:13:30 +0530

    fixed: sidebar responsiveness added

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -13,7 +13,7 @@ import { DateRangeFilter } from "../dateRangeFilter/DateRangeFilter";
 import { useAppStore } from "@/store/useAppStore";
 import { fetchSymbols } from "@/lib/binance";
 import { Filters } from "@/types/store.types";
-import { ThemeToggle } from "@/components/uielements/themeToggle/theme-toggle";
+import { ThemeToggle } from "../uielements/themeToggle/ThemeToggle";
 
 function SymbolSelector({
   filters,
@@ -28,7 +28,7 @@ function SymbolSelector({
 }) {
   return (
     <div className="space-y-3">
-      <label className="text-sm font-medium text-muted-secondary tracking-wide uppercase block">
+      <label className="text-xs 2xl:text-sm font-medium text-muted-secondary tracking-wide uppercase block">
         Trading Pair
       </label>
       <Select
@@ -39,7 +39,7 @@ function SymbolSelector({
         <SelectTrigger
           className={`w-full rounded border border-surface-border bg-background-input px-3 py-2.5 ${
             filters.symbol ? "text-brand" : "text-ash"
-          } text-base font-medium transition-all duration-200 hover:border-brand/30 focus:border-brand focus:ring-1 focus:ring-brand/20 h-11!`}
+          } text-xs 2xl:text-base font-medium transition-all duration-200 hover:border-brand/30 focus:border-brand focus:ring-1 focus:ring-brand/20 h-8! 2xl:h-11!`}
         >
           <SelectValue placeholder="Choose trading pair" />
         </SelectTrigger>
@@ -80,20 +80,20 @@ function FilterCheckboxes({
   const options = ["volatility", "liquidity", "performance"];
 
   return (
-    <div>
+    <div className="space-y-4">
       {options.map((key) => (
-        <div key={key} className="flex items-center gap-3 py-3">
+        <div key={key} className="flex items-center gap-3">
           <Checkbox
             id={key}
             checked={!!filters[key as keyof typeof filters]}
             onCheckedChange={(checked) =>
               updateFilter(key as keyof typeof filters, checked)
             }
-            className="h-6 w-6 [&_svg]:h-4 [&_svg]:w-4 [&_svg]:stroke-brand [&_svg]:!fill-transparent border-2 border-surface-border data-[state=checked]:bg-transparent data-[state=checked]:border-surface-border"
+            className="h-5 w-5 2xl:h-6 2xl:w-6 [&_svg]:h-3 [&_svg]:w-3 2xl:[&_svg]:h-4 2xl:[&_svg]:w-4 [&_svg]:stroke-brand [&_svg]:!fill-transparent border-2 border-surface-border data-[state=checked]:bg-transparent data-[state=checked]:border-surface-border"
           />
           <label
             htmlFor={key}
-            className="text-xl font-medium capitalize leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-label"
+            className="text-sm 2xl:text-lg font-medium capitalize leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-label"
           >
             {key}
           </label>
@@ -129,15 +129,15 @@ export default function Sidebar() {
   };
 
   return (
-    <aside className="w-sm bg-background-sidebar text-label p-6 flex flex-col gap-8 rounded-xl shadow-2xl border border-surface-border backdrop-blur-sm">
-      <div className="flex items-center justify-between pb-4">
+    <aside className="max-w-sm w-xs 2xl:w-sm bg-background-sidebar text-label p-5 2xl:p-6 flex flex-col gap-8 xl:gap-10 2xl:gap-12 rounded-xl border border-surface-border backdrop-blur-sm">
+      <div className="flex items-center justify-between">
         <div className="flex items-center gap-1">
-          <div className="w-8 h-8 md:w-6 md:h-6 antialiased">
+          <div className="relative w-5 h-5 2xl:w-6 2xl:h-6 antialiased">
             <Image 
               src="/logo.svg" 
               alt="Coindra Logo"
-              height={20}
-              width={20}
+              fill
+              className="object-contain"
               loading="lazy"
             />
           </div>
@@ -146,7 +146,7 @@ export default function Sidebar() {
         <ThemeToggle />
       </div>
 
-      <div className="space-y-6">
+      <div className="space-y-7">
         <SymbolSelector
           filters={filters}
           symbols={symbols || []}
@@ -155,14 +155,14 @@ export default function Sidebar() {
         />
 
         <div className="space-y-3">
-          <label className="text-sm font-medium text-muted-secondary tracking-wide uppercase block">
+          <label className="text-xs 2xl:text-sm font-medium text-muted-secondary tracking-wide uppercase block">
             Data Filters
           </label>
           <FilterCheckboxes filters={filters} updateFilter={updateFilter} />
         </div>
 
         <div className="space-y-3">
-          <label className="text-sm font-medium text-muted-secondary tracking-wide uppercase block">
+          <label className="text-xs 2xl:text-sm font-medium text-muted-secondary tracking-wide uppercase block">
             Date Range
           </label>
           <DateRangeFilter
