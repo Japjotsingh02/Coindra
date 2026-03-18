c60949718fa0475ecf3c7726eea99db11fd12452
Japjotsingh02 <sjapjot2002@gmail.com>
2026-03-18 14:00:00 +0530

    fixed: db build error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 
 export const prisma =
-  globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['error', 'warn'],
+    // log: ['error', 'warn'],
+    adapter,
   })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
